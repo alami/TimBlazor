2cc7ebbaa7acc48fbcdc0b8b3b5e68f003881a2b
tas_admin <tas_admin@hq.int.unesco.org>
2024-04-21 15:55:01 +0500

    add ch06 Component 49

---
+++ TimBlazor/Components/Pages/Tim/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TimBlazor.Components.Pages.Tim
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
