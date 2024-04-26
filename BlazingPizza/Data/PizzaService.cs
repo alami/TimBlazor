5c218ec9f575abc70c09b6886ce56ede4ed90016
tas_admin <tas_admin@hq.int.unesco.org>
2024-04-26 14:15:41 +0500

    add BlazingPizza DB via SQLite & MVC API

---
+++ BlazingPizza/Data/PizzaService.cs
@@ -0,0 +1,16 @@
+﻿using BlazingPizza.Components.Pages;
+using BlazingPizza.Model;
+using System.Xml.Linq;
+
+namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        public Task<Pizza[]> GetPizzasAsync()
+        {
+            // Call your data access technology here
+            throw new Exception();
+            //return null;
+        }
+    }
+}
--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -1,10 +1,16 @@
 using BlazingPizza.Components;
+using BlazingPizza.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddSingleton<PizzaService>();
+
+builder.Services.AddHttpClient();
+builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddMvc();
 
 var app = builder.Build();
 
@@ -23,5 +29,18 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
