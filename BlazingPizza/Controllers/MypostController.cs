698c4304da11565cfaf011c992482f1b02ff93e0
tas_admin <tas_admin@hq.int.unesco.org>
2024-05-01 18:07:42 +0500

    fin v3

--- BlazingPizza/Controllers/MypostController.cs
+++ BlazingPizza/Controllers/MypostController.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlazingPizza.Controllers
 {
-    [Route("mypost")]
+    [Route("[controller]")]
     [ApiController]
     public class MypostController : Controller
     {
