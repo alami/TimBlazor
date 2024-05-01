bcac40691292c564fca6ea96b53456d4ecbdc9d3
tas_admin <tas_admin@hq.int.unesco.org>
2024-05-01 14:51:26 +0500

    fin v2

---
+++ BlazingPizza/Controllers/MyController.cs
@@ -0,0 +1,24 @@
+﻿using BlazingPizza.Data;
+using BlazingPizza.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers
+{
+    [Route("my")]
+    [ApiController]
+    public class MyController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+        public MyController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetMy()
+        {
+            var tmp = (await _db.Specials.ToListAsync()).OrderBy(s => s.BasePrice).ToList();
+            return tmp;
+        }
+    }
+}
---
+++ BlazingPizza/Controllers/MypostController.cs
@@ -0,0 +1,39 @@
+﻿using BlazingPizza.Data;
+using BlazingPizza.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BlazingPizza.Controllers
+{
+    [Route("mypost")]
+    [ApiController]
+    public class MypostController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public MypostController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Order order)
+        {
+            order.CreatedTime = DateTime.Now;
+
+            // Enforce existence of Pizza.SpecialId and Topping.ToppingId
+            // in the database - prevent the submitter from making up
+            // new specials and toppings
+            foreach (var pizza in order.Pizzas)
+            {
+                pizza.SpecialId = pizza.Special.Id;
+                pizza.Special = null;
+            }
+
+            _db.Orders.Attach(order);
+            await _db.SaveChangesAsync();
+
+            return order.OrderId;
+        }
+    }
+}
