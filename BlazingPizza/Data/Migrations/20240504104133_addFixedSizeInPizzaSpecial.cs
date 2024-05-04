4f026939d34be8d7850a715fcfeb596677aa7363
tas_admin <tas_admin@hq.int.unesco.org>
2024-05-04 16:05:28 +0500

    07 05 migration, add fix size pizza end remove slider

---
+++ BlazingPizza/Data/Migrations/20240504104133_addFixedSizeInPizzaSpecial.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazingPizza.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFixedSizeInPizzaSpecial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FixedSize",
+                table: "Specials",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FixedSize",
+                table: "Specials");
+        }
+    }
+}
